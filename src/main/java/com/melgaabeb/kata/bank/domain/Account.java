0510a2558b0eb295e437f833aef0aedac00d93ac
el gaabeb <elgaabeb.mohamed@gmail.com>
2021-03-11 19:00:14 +0100

    added withdrawal feature implementation

--- src/main/java/com/melgaabeb/kata/bank/domain/Account.java
+++ src/main/java/com/melgaabeb/kata/bank/domain/Account.java
@@ -4,8 +4,25 @@ public class Account {
 
     private float balance;
 
+    public Account() {
+    }
+
+    public Account(float balance) {
+        this.balance = balance;
+    }
+
     public void deposit(float amount) {
         balance += amount;
+    }
+
+    public Integer withdrawal(float amount) {
+        Integer operationStatus = 0;
+        if (amount > balance) {
+            operationStatus = 1;
+        } else {
+            balance -= amount;
+        }
+        return operationStatus;
     }
 
     public float getBalance() {
