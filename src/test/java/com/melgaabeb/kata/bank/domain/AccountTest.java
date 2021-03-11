f976f7f7cd2e8d1d713256658ded21dbe4d83af5
el gaabeb <elgaabeb.mohamed@gmail.com>
2021-03-12 00:12:50 +0100

    Added test case for US 3 feature check operations

--- src/test/java/com/melgaabeb/kata/bank/domain/AccountTest.java
+++ src/test/java/com/melgaabeb/kata/bank/domain/AccountTest.java
@@ -1,9 +1,40 @@
 package com.melgaabeb.kata.bank.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.melgaabeb.kata.bank.ui.ConsolerDisplayer;
+import com.melgaabeb.kata.bank.ui.Displayer;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
 
 public class AccountTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private String expetedDate;
+    Displayer consoleDisplayerMock;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        expetedDate = dateFormat.format(new Date());
+
+        consoleDisplayerMock = mock(ConsolerDisplayer.class);
+
+        doAnswer(invocation -> {
+            Object arg0 = invocation.getArgument(0);
+            System.out.println(arg0);
+            return null;
+        }).when(consoleDisplayerMock).display(any(String.class));
+    }
 
     @Test
     public void Given_An_Account_When_Deposit_Then_Amount_is_added() {
@@ -24,5 +55,26 @@ public class AccountTest {
         // Then
         assertEquals(7, account.getBalance(), 0);
         assertEquals(0, operationStatus, 0);
+    }
+
+    @Test
+    public void Given_An_Account_When_Check_Operations_Then_Operation_History_is_displayed() {
+
+        // Given
+        Account account = new Account(12, consoleDisplayerMock);
+        account.deposit(26);
+        account.deposit(37);
+        account.withdrawal(15);
+
+       // When
+        account.checkOperations();
+
+        // Then
+        assertEquals("operation || date || amount || balance\n" +
+                "deposit || "+ expetedDate +" || 12.0 || 12.0\n" +
+                "deposit || "+ expetedDate +" || 26.0 || 38.0\n" +
+                "deposit || "+ expetedDate +" || 37.0 || 75.0\n" +
+                "withdrawal || "+ expetedDate +" || 15.0 || 60.0\n", outputStreamCaptor.toString());
+        verify(consoleDisplayerMock, times(5)).display(any(String.class));
     }
 }
